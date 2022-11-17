bcdefe9a47a538dbb31604ff0c61685fbb3f26f2
Terence Grigoruk <terence.grigoruk@gmail.com>
2022-11-17 13:41:19 -0800

    Add createBackground(), createPlayArea(), createPlayer(), spawnEnemyTextBubble()

--- src/main/java/ca/bcit/comp2522/termproject/emoji/EmojiApplication.java
+++ src/main/java/ca/bcit/comp2522/termproject/emoji/EmojiApplication.java
@@ -1,8 +1,8 @@
 package ca.bcit.comp2522.termproject.emoji;
 
 import javafx.application.Application;
-import javafx.geometry.Rectangle2D;
 import javafx.scene.Group;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -27,6 +27,7 @@ public class EmojiApplication extends Application {
     public final static int EMOJI_SIZE = 40;
     public final static Random RNG = new Random();
 
+    private final static Group root = new Group();
     /**
      * Displays an image centered in a window.
      *
@@ -34,43 +35,62 @@ public class EmojiApplication extends Application {
      */
     @Override
     public void start(final Stage primaryStage) {
+        boolean gameNotOver = true;
 
-        // create the background image
-        Image backgroundImage = new Image("blue-pink-geometric.jpg");
-        ImageView imageView = new ImageView(backgroundImage);
-//        imageView.setViewport(new Rectangle2D(0, 0, APP_WIDTH, APP_HEIGHT));
-        imageView.setFitHeight(APP_HEIGHT);
-        imageView.setPreserveRatio(true);
-        imageView.setSmooth(true);
-        imageView.setCache(true);
-
-        // create the play area
-        Rectangle playArea = new Rectangle(
-                (APP_WIDTH - PLAY_AREA_SIZE) / 2,
-                (APP_HEIGHT - PLAY_AREA_SIZE) / 2,
-                PLAY_AREA_SIZE,
-                PLAY_AREA_SIZE);
-        playArea.setStroke(Color.BLACK);
-        playArea.setFill(new Color(1,1,1,0.8));
-        playArea.setArcHeight(10);
-        playArea.setArcWidth(10);
-
-        Group enemy = new EnemyTextBubbleFactory("left",200, "angry");
-
-
-
-        Group root = new Group(imageView, playArea, enemy);
-//        root.getChildren().add(new Enemy("angry"));
+        createBackground();
+        createPlayArea();
+        createPlayer();
+        spawnEnemyTextBubble();
 
         Scene scene = new Scene(root, APP_WIDTH, APP_HEIGHT);
         primaryStage.setResizable(false);
         primaryStage.setTitle("Untitled Emoji Game");
         primaryStage.setScene(scene);
         primaryStage.show();
+
+        // main game loop
+//        while (gameNotOver) {
+//            gameNotOver = player.isAlive();
+//        }
     }
 
     public static void main(final String[] args) {
         launch();
+    }
+
+    private void createBackground() {
+        Image backgroundImage = new Image("blue-pink-geometric.jpg");
+        ImageView backgroundImageView = new ImageView(backgroundImage);
+        backgroundImageView.setFitHeight(APP_HEIGHT);
+        backgroundImageView.setPreserveRatio(true);
+        backgroundImageView.setSmooth(true);
+        backgroundImageView.setCache(true);
+        root.getChildren().add(backgroundImageView);
+    }
+
+    private void createPlayArea() {
+        int playAreaLeft = (APP_WIDTH - PLAY_AREA_SIZE) / 2;
+        int playAreaTop = (APP_HEIGHT - PLAY_AREA_SIZE) / 2;
+        Rectangle playArea = new Rectangle(
+                playAreaLeft,
+                playAreaTop,
+                PLAY_AREA_SIZE,
+                PLAY_AREA_SIZE);
+        playArea.setStroke(Color.BLACK);
+        playArea.setFill(new Color(1,1,1,0.8));
+        playArea.setArcHeight(10);
+        playArea.setArcWidth(10);
+        root.getChildren().add(playArea);
+    }
+
+    public void createPlayer() {
+        Group player = new Player(APP_WIDTH / 2, APP_HEIGHT / 2);
+        root.getChildren().add(player);
+
+    }
+    public void spawnEnemyTextBubble() {
+        EnemyTextBubble enemy = new EnemyTextBubble("left",200, "angry");
+        root.getChildren().add(enemy);
     }
 }
 
