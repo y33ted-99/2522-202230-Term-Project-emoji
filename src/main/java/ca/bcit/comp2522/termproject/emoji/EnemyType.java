2eace6501e1ac825638b699937697df9913028ab
Terence Grigoruk <terence.grigoruk@gmail.com>
2022-11-18 12:04:46 -0800

    Add more enemy types

--- src/main/java/ca/bcit/comp2522/termproject/emoji/EnemyType.java
+++ src/main/java/ca/bcit/comp2522/termproject/emoji/EnemyType.java
@@ -4,7 +4,18 @@ package ca.bcit.comp2522.termproject.emoji;
  * An enemy emoji type.
  */
 public enum EnemyType implements EmojiType {
-    ANGRY("angry.png", "OMFG");
+    ANGRY("angry.png", "OMFG"),
+    DROOL("drooling_face.png", "DURR"),
+    VOMIT("face_vomiting.png", "BARF"),
+    SWEAR("face_with_symbols_on_mouth.png", "&$!#%"),
+    GRIMACE("grimacing.png", "OOF"),
+    HALO("innocent.png", "WWJD"),
+    SICK("nauseated_face.png", "YUCK"),
+    RAGE("rage.png", "GRRR"),
+    ROFL("rofl.png", "ROFL"),
+    SOB("sob.png", "WAAH"),
+    THINK("thinking_face.png", "HMMM"),
+    ;
 
     private String filename;
     private String phrase;
