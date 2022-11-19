6a117c1cac708abc08a88291c4d59e7d751d9408
Brian Mak <brianmak.blue@gmail.com>
2022-11-19 12:48:44 -0800

    added movement

--- src/main/java/ca/bcit/comp2522/termproject/emoji/EmojiApplication.java
+++ src/main/java/ca/bcit/comp2522/termproject/emoji/EmojiApplication.java
@@ -1,16 +1,26 @@
 package ca.bcit.comp2522.termproject.emoji;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
+import javafx.collections.ObservableList;
 import javafx.scene.Group;
+import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
+/*import javax.swing.*;*/
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.ObjectInputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Random;
@@ -37,6 +47,18 @@ public class EmojiApplication extends Application {
 
     public final static int MARGIN_X = (APP_WIDTH - PLAY_AREA_SIZE) / 2;
     public final static int MARGIN_Y = (APP_HEIGHT - PLAY_AREA_SIZE) / 2;
+
+    public enum Direction {
+        UP, DOWN, LEFT, RIGHT
+    }
+
+    private boolean running = false;
+
+    private boolean move = true;
+    private Timeline timeline = new Timeline();
+
+    private Direction direction = Direction.UP;
+
     /**
      * Displays an image centered in a window.
      *
@@ -48,7 +70,6 @@ public class EmojiApplication extends Application {
 
         createBackground();
         createPlayArea();
-        createPlayer();
 
         setUpTextBubbleArrays();
 
@@ -57,17 +78,42 @@ public class EmojiApplication extends Application {
 
 
 
-        Scene scene = new Scene(root, APP_WIDTH, APP_HEIGHT);
+        /*Scene scene = new Scene(root, APP_WIDTH, APP_HEIGHT);*/
+        Scene scene = new Scene(content());
+        scene.setOnKeyPressed(event -> {
+            switch (event.getCode()) {
+                case W:
+                    if (direction != Direction.DOWN)
+                        direction = Direction.UP;
+                    break;
+                case S:
+                    if (direction != Direction.UP)
+                        direction = Direction.DOWN;
+                    break;
+                case A:
+                    if (direction != Direction.RIGHT)
+                        direction = Direction.LEFT;
+                    break;
+                case D:
+                    if (direction != Direction.LEFT)
+                        direction = Direction.RIGHT;
+                    break;
+            }
+        });
         primaryStage.setResizable(false);
         primaryStage.setTitle("Untitled Emoji Game");
         primaryStage.setScene(scene);
         primaryStage.show();
+        startGame();
+
+
 
         // main game loop
 //        while (gameNotOver) {
 //            gameNotOver = player.isAlive();
 //        }
     }
+
 
     public static void main(final String[] args) {
         launch();
@@ -77,7 +123,8 @@ public class EmojiApplication extends Application {
         try (InputStream is = Files.newInputStream(Paths.get("resources/bg/blue-pink-background.jpg"))) {
             ImageView backgroundImageView = new ImageView(new Image(is));
             backgroundImageView.setFitHeight(APP_HEIGHT);
-            backgroundImageView.setPreserveRatio(true);
+            backgroundImageView.setFitWidth(APP_WIDTH);
+            backgroundImageView.setPreserveRatio(false);
             backgroundImageView.setCache(true);
             root.getChildren().add(backgroundImageView);
         } catch (IOException e) {
@@ -130,5 +177,69 @@ public class EmojiApplication extends Application {
         enemyCount++;
         root.getChildren().add(enemy);
     }
+
+    private Parent content() {
+
+        /*APP_WIDTH / 2, (int) (APP_HEIGHT * 0.75)*/
+
+        Group player = new Player(APP_WIDTH / 2, (int) (APP_HEIGHT * 0.75));
+        root.getChildren().add(player);
+        if (move) {
+            KeyFrame frame = new KeyFrame(Duration.millis(75), event -> {
+                if (!running)
+                    return;
+
+                switch (direction) {
+                    case UP:
+                        player.setTranslateX(player.getTranslateX());
+                        player.setTranslateY(player.getTranslateY() - EMOJI_SIZE);
+                        break;
+
+                    case DOWN:
+                        player.setTranslateX(player.getTranslateX());
+                        player.setTranslateY(player.getTranslateY() + EMOJI_SIZE);
+                        break;
+
+                    case LEFT:
+                        player.setTranslateX(player.getTranslateX() - EMOJI_SIZE);
+                        player.setTranslateY(player.getTranslateY());
+                        break;
+
+                    case RIGHT:
+                        player.setTranslateX(player.getTranslateX() + EMOJI_SIZE);
+                        player.setTranslateY(player.getTranslateY());
+                        break;
+                }
+
+                if (player.getTranslateX() < 0 || player.getTranslateX() >= MARGIN_X || player.getTranslateY() < 0 ||
+                        player.getTranslateY() >= MARGIN_Y) {
+                    /*resetPosition();*/
+                }
+
+            });
+
+            timeline.getKeyFrames().add(frame);
+            timeline.setCycleCount(Timeline.INDEFINITE);
+
+        }
+        return root;
+    }
+
+    private void startGame() {
+        direction = Direction.UP;
+        timeline.play();
+        running = true;
+    }
+
+    /*private void stopGame() {
+        running = false;
+        timeline.stop();
+        playa.clear();
+    }
+    private void resetPosition() {
+        stopGame();
+        startGame();
+    }*/
 }
+
 
--- src/main/java/ca/bcit/comp2522/termproject/emoji/Player.java
+++ src/main/java/ca/bcit/comp2522/termproject/emoji/Player.java
@@ -1,7 +1,18 @@
 package ca.bcit.comp2522.termproject.emoji;
 
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
 public class Player extends Entity{
+
     public Player(final int xPosition, final int yPosition) {
         super(xPosition, yPosition, "player/" + PlayerState.SMILEY.getFilename());
     }
 }
+
+
