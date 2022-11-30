391ad38bd05b15e7b42c15207ba9f1e243f3ad27
Terence Grigoruk <terence.grigoruk@gmail.com>
2022-11-29 19:59:30 -0800

    Refactor GameTimer and MainMenu logic into seperate classes

--- src/main/java/ca/bcit/comp2522/termproject/emoji/EmojiApp.java
+++ src/main/java/ca/bcit/comp2522/termproject/emoji/EmojiApp.java
@@ -2,22 +2,11 @@ package ca.bcit.comp2522.termproject.emoji;
 
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
-import javafx.application.Platform;
-import javafx.beans.binding.Bindings;
 import javafx.geometry.Bounds;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
-import javafx.scene.paint.CycleMethod;
-import javafx.scene.paint.LinearGradient;
-import javafx.scene.paint.Stop;
-import javafx.scene.shape.Rectangle;
-import javafx.scene.text.Font;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.util.ArrayList;
@@ -56,15 +45,17 @@ public class EmojiApp extends Application {
     private static Player player;
     private static TextBubbleGroup leftTextBubbleGroup;
     private static TextBubbleGroup rightTextBubbleGroup;
-    private static final List<GameItem> gameItems = new ArrayList<>();
+    private static List<GameItem> gameItems;
     private static boolean gameOver = false;
-    private static int score = 0;
+    private static AnimationTimer timer;
+    private static MainMenu mainMenu;
+    private static GameTimer gameTimer;
+    private static long startTime;
 
-    private static VBox box;
 
-    private Integer time = 0;
-
-
+    /*
+     * Creates all the content to be drawn to screen.
+     */
     private Parent createContent() {
         root = new Pane();
         root.setPrefSize(APP_WIDTH, APP_HEIGHT);
@@ -72,51 +63,42 @@ public class EmojiApp extends Application {
         Node background = PlayArea.createBackground();
         Node playArea = PlayArea.createPlayArea();
         Node letterBar = LetterBar.createLetterBar();
+        mainMenu = new MainMenu();
+        gameTimer = new GameTimer();
 
         player = createPlayer();
         leftTextBubbleGroup = new TextBubbleGroup(Side.LEFT);
         rightTextBubbleGroup = new TextBubbleGroup(Side.RIGHT);
+        gameItems = new ArrayList<>();
         root.getChildren().addAll(
                 background,
                 playArea,
                 letterBar,
                 player,
                 leftTextBubbleGroup,
-                rightTextBubbleGroup);
-
-        showScore("SCORE: ");
-        final Text text = new Text (time.toString());
-        text.setStroke(Color.BLACK);
-        text.setFont(Font.font(25));
-        final StackPane stack2 = new StackPane();
-        stack2.getChildren().addAll(text);
-        stack2.setLayoutX(700);
-        stack2.setLayoutY(70);
-
+                rightTextBubbleGroup,
+                mainMenu,
+                gameTimer);
 
         // Main game loop
-        AnimationTimer timer = new AnimationTimer() {
+        timer = new AnimationTimer() {
             @Override
             public void handle(final long now) {
                 onUpdate(now);
-                text.setText(time.toString());
-                time++;
-                root.getChildren().removeAll(box);
             }
         };
 
-        box = new VBox(
-                20,
-                new MenuItem("START", () -> timer.start()),
-                new MenuItem("HIGHSCORE", () -> {}),
-                new MenuItem("QUIT", () -> Platform.exit())
-        );
-
-        box.setTranslateX(380);
-        box.setTranslateY(380);
-        root.getChildren().addAll(box, stack2);
-
         return root;
+    }
+
+    public static void startGame() {
+        removeFromRootScene(mainMenu);
+        startTime = System.nanoTime();
+        timer.start();
+    }
+
+    public static long getStartTime() {
+        return startTime;
     }
 
     /*
@@ -136,7 +118,7 @@ public class EmojiApp extends Application {
         rightTextBubbleGroup.update();
         checkGameItems();
         gameItems.forEach(GameItem::update);
-
+        gameTimer.update(now);
         if (!gameOver) {
             player.move();
         }
@@ -152,9 +134,7 @@ public class EmojiApp extends Application {
 
         Scene scene = new Scene(createContent());
 
-        scene.setOnMouseMoved(event -> {
-            player.moveToMouse(event);
-        });
+        scene.setOnMouseMoved(event -> player.moveToMouse(event));
         scene.setOnMouseClicked(event -> {
             leftTextBubbleGroup.mouseClickHandler(event);
             rightTextBubbleGroup.mouseClickHandler(event);
@@ -239,16 +219,31 @@ public class EmojiApp extends Application {
     /**
      * Adds points to the player's score.
      *
-     * @param points
+     * @param points points to add to player score as int
      */
     public static void addToScore(final int points) {
         player.addToScore(points);
     }
 
+    /**
+     * Returns the player's score.
+     *
+     * @return player score as int
+     */
+    public static int getPlayerScore() {
+        return player.getScore();
+    }
+
+    /**
+     * Increments the number of text bubbles a player has popped.
+     */
     public static void incrementPlayerPoppedBubbles() {
         player.incrementPoppedBubbles();
     }
 
+    /**
+     * Spawns an item.
+     */
     public static void spawnItem() {
         ItemType type = ItemType.values()[new Random().nextInt(ItemType.values().length)];
         GameItem gameItem = GameItem.getInstance(type);
@@ -256,52 +251,19 @@ public class EmojiApp extends Application {
         addToRootScene(gameItem);
     }
 
+    /**
+     * Checks games and if acquired by player removes from game and adds points to player.
+     */
     public static void checkGameItems() {
-        Iterator iterator = gameItems.iterator();
+        Iterator<GameItem> iterator = gameItems.iterator();
         while (iterator.hasNext()) {
-            GameItem gameItem = (GameItem) iterator.next();
+            GameItem gameItem = iterator.next();
             if (!gameItem.isAlive()) {
                 removeFromRootScene(gameItem);
                 EmojiApp.addToScore(GameItem.POINTS_PER_ITEM);
                 iterator.remove();
             }
         }
-    }
-
-    private static class MenuItem extends StackPane {
-        MenuItem(String name, Runnable action) {
-            LinearGradient gradient = new LinearGradient(
-                    0.5, 0.5, 1, 0.5, true, CycleMethod.NO_CYCLE,
-                    new Stop(0.8, Color.web("Pink", 0.5)),
-                    new Stop(1, Color.web("white", 0.5)));
-            Rectangle bg = new Rectangle(250, 30, gradient);
-
-
-            Text text = new Text(name);
-            text.setFont(Font.font(25));
-            text.fillProperty().bind(
-                    Bindings.when(hoverProperty()).then(Color.WHITE).otherwise(Color.BLACK)
-            );
-
-            setOnMouseClicked(e -> action.run());
-
-
-            getChildren().addAll(bg, text);
-        }
-
-    }
-
-    private void showScore (String count) {
-        final Text score = new Text(count);
-        score.setStroke(Color.BLACK);
-        score.setFont(Font.font(25));
-        final StackPane stack1 = new StackPane();
-        stack1.getChildren().addAll(score);
-        stack1.setLayoutX(600);
-        stack1.setLayoutY(70);
-
-        root.getChildren().addAll(stack1);
-
     }
 }
 
